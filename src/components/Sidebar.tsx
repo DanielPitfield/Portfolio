a64da21b7bb026ecf1adb75cd7f040627874c655
Daniel Pitfield <danielpitfield1@gmail.com>
2023-01-18 04:16:45 +0000

    sidebar - open links in new tab

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,7 +5,6 @@ import { MdEmail } from "react-icons/md";
 import { BsGithub } from "react-icons/bs";
 import { BsFileEarmarkTextFill } from "react-icons/bs";
 
-
 type SidebarItem = { name: string; link: string; icon: IconType };
 
 const items: SidebarItem[] = [
@@ -19,7 +18,7 @@ const Sidebar = () => {
     <ul className={styles.wrapper}>
       {items.map((item) => (
         <li key={item.name} className={styles.item}>
-          <a className={styles.link} href={item.link}>
+          <a className={styles.link} href={item.link} target="_blank" rel="noreferrer">
             <item.icon className={styles.icon} />
             {item.name}
           </a>
