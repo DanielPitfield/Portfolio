ff2bf4adaf8c69e216d6d7b05d8939eac36fe6e0
DanielPitfield <danielpitfield1@gmail.com>
2023-09-01 21:56:47 +0100

    Fix ProjectImage component

--- src/components/ProjectImage.tsx
+++ src/components/ProjectImage.tsx
@@ -1,15 +1,16 @@
+import { ProjectConfig } from "../data/projects";
 import styles from "../styles/Project.module.scss";
 
 interface ProjectImageProps {
-  image: string;
-  name: string;
-  demoConfig: { isDemoEnabled: false } | { isDemoEnabled: true; link: string };
-  viewCodeConfig: { isViewCodeEnabled: false; reason?: string } | { isViewCodeEnabled: true; link: string };
+  image: ProjectConfig["image"];
+  name: ProjectConfig["name"];
+  demoConfig: ProjectConfig["demoConfig"];
+  viewCodeConfig: ProjectConfig["viewCodeConfig"];
 }
 
 const ProjectImage = (props: ProjectImageProps) => {
   // What URL should be linked to?
-  function getLink(): string | undefined {
+  function getLink(): URL | undefined {
     // Live Demo link as first priority
     if (props.demoConfig.isDemoEnabled) {
       return props.demoConfig.link;
@@ -24,7 +25,7 @@ const ProjectImage = (props: ProjectImageProps) => {
 
   return (
     <div className={styles.imageWrapper}>
-      <a className={styles.imageLink} tabIndex={-1} href={getLink()} target="_blank" rel="noreferrer">
+      <a className={styles.imageLink} tabIndex={-1} href={getLink()?.toString()} target="_blank" rel="noreferrer">
         <img className={styles.image} src={props.image} alt={props.name} />
       </a>
     </div>
